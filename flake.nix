2b538cfacb7aa94d365ad2f395409896bf3f193a
tedius-git <diegominasantos@gmail.com>
2025-01-10 13:44:17 -0600

    batman

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkg.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgsold.url = "github:nixos/nixpkgs?ref=nixos-21.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.hp = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
