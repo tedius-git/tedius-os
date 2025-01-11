087f318b1ed7d50842b6118fccacf100691bcb74
tedius-git <diegominasantos@gmail.com>
2025-01-10 20:36:00 -0600

    Add home manager to flake

--- flake.nix
+++ flake.nix
@@ -5,11 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgsold.url = "github:nixos/nixpkgs?ref=nixos-21.11";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   } @ inputs: {
     nixosConfigurations.hp = nixpkgs.lib.nixosSystem {
@@ -17,6 +22,12 @@
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tedius = import ./home.nix;
+        }
       ];
     };
   };
